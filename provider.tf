0c0a357e31da3c9af0e028f6c50f8d0bf58cf627
Calvine Otieno <nyarangaotieno@gmail.com>
2022-07-23 22:57:01 +0300

    chore: add cloudflare and helm providers
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- provider.tf
+++ provider.tf
@@ -4,3 +4,19 @@ provider "google" {
     region = var.region
     zone = var.zone
 }
+
+provider "cloudflare" {
+  email = var.cloudflare_email
+  account_id = var.cloudflare_account_id
+  api_key = var.cloudflare_token
+}
+
+data "google_client_config" "default" {}
+
+provider "helm" {
+  kubernetes {
+    host                   = google_container_cluster.app_server.endpoint
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.app_server.master_auth.0.cluster_ca_certificate)
+  }
+}
