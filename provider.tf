f5668d0a19eeaaa5e4d4a36e846163d1cbf39117
Calvine Otieno <nyarangaotieno@gmail.com>
2022-07-23 23:06:32 +0300

    chore: add kubernetes and random terraform providers
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- provider.tf
+++ provider.tf
@@ -20,3 +20,13 @@ provider "helm" {
     cluster_ca_certificate = base64decode(google_container_cluster.app_server.master_auth.0.cluster_ca_certificate)
   }
 }
+
+provider "kubernetes" {
+  host  = "https://${google_container_cluster.primary.endpoint}" 
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+}
+
+provider "random" {
+  
+}
